f350ab6aba3c447495fa1c26954aceda17f881ef
Exiler12 <hoangbui036@gmail.com>
2023-06-02 16:21:32 +0700

    third

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Category::factory(5)->create();
+        \App\Models\Product::factory(20)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
